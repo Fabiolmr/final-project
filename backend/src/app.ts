ea72d9e95d1277c4ee7120a9f9d2f0951a7bfcd2
Fabiolmr <fabiolourenco395@gmail.com>
2026-07-01 14:48:11 -0300

    inicio da lógica de autenticação do usuário

---
+++ backend/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from 'cors'
+import {authRoutes} from './routes/auth.routes';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use('/auth', authRoutes);
+
+export {app};
